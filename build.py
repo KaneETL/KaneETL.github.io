2c9731dc17a3b18d38b38b0314c657446671612f
KaneETL <kturbittlewis@gmail.com>
2021-09-09 16:29:05 +0100

    Updated workflow with enviroment variables

--- build.py
+++ build.py
@@ -1,7 +1,11 @@
 from staticjinja import Site
-
+import os
 
 if __name__ == "__main__":
+    prod = os.getenv("prod")
     site = Site.make_site(outpath="dist")
     # enable automatic reloading
-    site.render(use_reloader=True)
+    if prod:
+        site.render(use_reloader=False)
+    else:
+        site.render(use_reloader=True)

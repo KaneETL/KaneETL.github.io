0cfbc2424364b8f100a163ed92723a5ec030c1e5
KaneETL <kturbittlewis@gmail.com>
2021-09-10 10:00:00 +0100

    Updated python script

--- build.py
+++ build.py
@@ -1,10 +1,19 @@
 from staticjinja import Site
 import os
+import shutil
 
 if __name__ == "__main__":
     prod = os.getenv("prod")
     site = Site.make_site(outpath="dist")
     # enable automatic reloading
+    src = "static"
+    dst = "dist/" + src
+    dir_check = os.path.exists(dst)
+
+    if dir_check == True:
+        shutil.rmtree(dst)
+
+    shutil.copytree(src, dst)
     if prod:
         site.render(use_reloader=False)
     else:
